c50548432f1374f7cea261e6756a293acc061840
Austin Hartzheim <inbox@austinhartzheim.me>
2017-11-11 23:22:32 +0000

    Added code to read the webcam and fix .gitignore.

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,31 @@ let v = new Vue({
         name: "World"
     }
 });
+
+let camera = new Vue({
+    el: "#camera-component",
+    template: `
+    <div>Camera
+      <video id="camera-display" width="393" height="295" preload autoplay loop muted controls></video>
+      <button v-on:click="init">Init Tracking</button>
+    </div>
+    `,
+    methods: {
+        init: function() {
+            navigator.mediaDevices.getUserMedia({video: {width: 1280, height: 720}})
+            .then(function(mediaStream) {
+                console.log('success; media stream:', mediaStream);
+                var videoElm = document.querySelector('#camera-display');
+                console.log('video elm:', videoElm);
+                videoElm.src = window.URL.createObjectURL(mediaStream);
+                console.log('video src:', videoElm.src);
+                videoElm.onloadedmetadata = function(e) {
+                    console.log('Loaded webcam metadata');
+                };
+            })
+            .catch(function (err) {
+                console.log('Error getting user media:', err);
+            });
+        }
+    }
+});
