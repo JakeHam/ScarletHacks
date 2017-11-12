bb9375a2e35373d1b90b2567dd0be208c1a13291
jakehamelhs@gmail.com <jakehamelhs@gmail.com>
2017-11-11 20:26:36 -0600

    Working Mapbox test. Do not kill me if it breaks your stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
-import App from './components/App.vue'
+import App from './App.vue'
+import vMapboxgl from './components/vMapboxgl.vue'
+
+Vue.component('v-mapboxgl', vMapboxgl)
 
 new Vue({
   el: '#app',
