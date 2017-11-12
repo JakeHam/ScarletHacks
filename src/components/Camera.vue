721ef1abdb49329134ba387eceeb442d0f97a39f
Austin Hartzheim <inbox@austinhartzheim.me>
2017-11-12 02:43:24 +0000

    Use tracking.js for face tracking.

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,40 +1,47 @@
 <template>
 <div>Camera
-  <video id="camera-display" width="393" height="295" preload autoplay loop muted controls></video>
+  <video id="camera-display" width="320" height="240" preload autoplay loop muted controls></video>
+  <canvas id="camera-canvas" width="320" height="240"></canvas>
   <button v-on:click="init">Init Tracking</button>
 </div>
 </template>
+
+<style>
+    #camera-display, #camera-canvas {
+        position: fixed;
+        right: 0px;
+        bottom: 0px;
+    }
+</style>
 
 <script>
 export default {
     name: 'camera',
     methods: {
         init: function() {
-            /*navigator.mediaDevices.getUserMedia({video: {width: 1280, height: 720}})
-            .then(function(mediaStream) {
-                console.log('success; media stream:', mediaStream);
-                var videoElm = document.querySelector('#camera-display');
-                console.log('video elm:', videoElm);
-                videoElm.src = window.URL.createObjectURL(mediaStream);
-                console.log('video src:', videoElm.src);
-                videoElm.onloadedmetadata = function(e) {
-                    console.log('Loaded webcam metadata');
-                };
-            })
-            .catch(function (err) {
-                console.log('Error getting user media:', err);
-            });*/
-
             var video = document.getElementById('camera-display');
-            //var canvas = document.getElementById('canvas');
-            //var context = canvas.getContext('2d');
+            var canvas = document.getElementById('camera-canvas');
+            var context = canvas.getContext('2d');
 
             var tracker = new tracking.ObjectTracker(['face']);
-            //tracker.setInitialScale(4);
-            //tracker.setStepSize(2);
-            //tracker.setEdgesDensity(0.1);
+            tracker.setInitialScale(4);
+            tracker.setStepSize(2);
+            tracker.setEdgesDensity(0.1);
 
-            //tracking.track('#camera-display', tracker, { camera: true });
+            tracking.track('#camera-display', tracker, { camera: true });
+
+            tracker.on('track', function(event) {
+                context.clearRect(0, 0, canvas.width, canvas.height);
+
+                event.data.forEach(function(rect) {
+                    context.strokeStyle = '#a64ceb';
+                    context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+                    context.font = '11px Helvetica';
+                    context.fillStyle = "#fff";
+                    context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+                    context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+                });
+            });
 
         }
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './components/App.vue'
 import Camera from './components/Camera.vue'
+import tracking from '../node_modules/tracking/build/tracking.js'
+import FaceData from './face_data.js'
 
 new Vue({
   el: '#app',
